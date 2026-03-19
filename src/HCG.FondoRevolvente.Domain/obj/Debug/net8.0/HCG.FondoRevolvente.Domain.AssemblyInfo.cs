c4e77afcfb467b2b31eef79b0c779877f2a3d0d6
j3susangar1ca <j3susangar1ca@gmail.com>
2026-03-19 02:54:39 -0600

    r

--- src/HCG.FondoRevolvente.Domain/obj/Debug/net8.0/HCG.FondoRevolvente.Domain.AssemblyInfo.cs
+++ src/HCG.FondoRevolvente.Domain/obj/Debug/net8.0/HCG.FondoRevolvente.Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HCG.FondoRevolvente.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ab3f76f0af22e24d69db379f98eac0a48bbbbf26")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dee2e7b2632be9fa59e939cd78e8949608a499d8")]
 [assembly: System.Reflection.AssemblyProductAttribute("HCG.FondoRevolvente.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HCG.FondoRevolvente.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
